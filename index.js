317228be9c942158480a1c40704d36d19c550ff3
Federico Brigante <bfred-it@users.noreply.github.com>
2016-06-24 00:11:28 +0700

    Remove dependency; simplify code

--- index.js
+++ index.js
@@ -1,16 +1,10 @@
 'use strict';
-import {on, off} from 'on-off';
-
-function getSelfRemovingHandler (element, type, listener, useCapture) {
-	return function selfRemoving() {
-		off(element, type, listener, useCapture);
-		off(element, type, selfRemoving, useCapture);
-	};
-}
 
 export default function once (element, type, listener, useCapture) {
-	let selfRemoving = getSelfRemovingHandler.apply(null, arguments);
-	on(element, type, listener, useCapture);
-	on(element, type, selfRemoving, useCapture);
-	return listener;
+	function selfRemoving() {
+		element.removeEventListener(type, listener, useCapture);
+		element.removeEventListener(type, selfRemoving, useCapture);
+	}
+	element.addEventListener(type, listener, useCapture);
+	element.addEventListener(type, selfRemoving, useCapture);
 }
